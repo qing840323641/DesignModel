5753eccd1fc85fa862d84a43a569145bf0323585
840323641@qq.com <admin>
2019-09-08 21:54:13 +0800

    修改-------

--- src/DesignModel/GuanChaZheMode/XiaoLi.java
+++ src/DesignModel/GuanChaZheMode/XiaoLi.java
@@ -16,8 +16,7 @@ public class XiaoLi implements Person{
     @Override
     public void getMessage(String s) {
         XiaoMei xiaoMei = new XiaoMei();
-        System.out.println(name + "接到了asdadwqeqwe小美的电话，内容是：" + s);
-
+        System.out.println(name + "接到美的电话，内容是：" + s);
     }
 }
 
