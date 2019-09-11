056e152f597416cad4c57885dc04a0e78f3f723b
840323641@qq.com <admin>
2019-09-11 14:00:05 +0800

    修改-11------

--- src/Test.java
+++ src/Test.java
@@ -8,7 +8,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        int[] arr = {1, 3, 54, 6, 8, 9, 4, 7, 4};
+        int[] arr = {1, 3, 54,11, 6, 8, 9, 4, 7, 4};
         me(arr);
     }
 
